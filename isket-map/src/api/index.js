97b010f7e5c699b5a9c7111e1e5c14485ecd4072
OldRoge <rogeriomrjr@icloud.com>
2022-05-10 21:53:02 -0300

    feat: create a provider to share api datas

--- isket-map/src/api/index.js
+++ isket-map/src/api/index.js
@@ -1,13 +1,32 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState, useEffect, useMemo } from 'react';
+import myContext from '../Components/MyContext';
 
-export default function getApi() {
-  const [render, setRender] = useState(null);
-  console.log(render);
+export default function getApi({ children }) {
+  const [dataMaps, setDataMaps] = useState([]);
 
   useEffect(() => {
-    axios.get(process.env.REACT_APP_API_GET).then((res) => {
-      setRender(res.data);
-    });
+    axios
+      .get(process.env.REACT_APP_API_GET)
+      .then((res) => {
+        setDataMaps(res.data);
+      });
   }, []);
+  /* Estava com problema de re-render em estados que não precisavam
+  ser renderizados novamente, resolvido com a referência a seguir: */
+
+  // https://blog.agney.dev/useMemo-inside-context/
+  const values = useMemo(() => ({
+    dataMaps,
+    setDataMaps,
+  }), [dataMaps]);
+
+  if (!dataMaps) return null;
+  return (
+    <myContext.Provider value={values}>
+      { children }
+    </myContext.Provider>
+  );
 }
